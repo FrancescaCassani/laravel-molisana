45278cace40c89d43bb09b0a422568c1b4adb4bc
Francesca <fracassani@gmail.com>
2021-01-05 17:52:08 +0100

    Dynamic menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,12 +16,16 @@
     </div>
 
     <div class="only-mobile">
-        <a href=""> <i @click="isOpen = !isOpen"  class="fas fa-bars"></i> </a>
-            {{-- <div class="menu">
-                <a href="/">Home</a>
-                <a href="{{ route('ricette') }} ">Le ricette</a>
-                <a href="{{ route('news') }} ">Novità</a>
-          </div> --}}
+        <a href="#"> 
+            <i @click="isOpen = !isOpen" class="fas fa-bars"></i> 
+        </a>
+            {{-- <transition name="fade" appear> --}}
+                <div v-if="isOpen" class="menu">
+                    <a href="/">Home</a>
+                    <a href="{{ route('ricette') }} ">Le ricette</a>
+                    <a href="{{ route('news') }} ">Novità</a>
+                </div>
+            {{-- <transition> --}}
     </div>
 </header>
 
